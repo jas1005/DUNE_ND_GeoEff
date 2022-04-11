0d82d7b85b297ad7241e23fd8c4fb90b6ccf8306
FlynnGuo <93301049+FlynnYGUO@users.noreply.github.com>
2022-04-11 16:39:45 -0400

    update

--- app/runGeoEffFDEvtSim_test.cpp
+++ app/runGeoEffFDEvtSim_test.cpp
@@ -457,7 +457,7 @@ int main(){
         // Evt vtx pos in unit: cm
         eff->setVertex( i_vtx_vx + i_ND_off_axis_pos, ND_Sim_mu_start_vy, ND_Sim_mu_start_vz );
         eff->setNewVertexBF(i_vtx_vx + i_ND_off_axis_pos, ND_Sim_mu_start_OnAxis_vy, ND_Sim_mu_start_OnAxis_vz);
-        eff->setMuEndV(ND_Sim_mu_end_vx[ientry],ND_Sim_mu_end_vy,ND_Sim_mu_end_vz);
+        //eff->setMuEndV(ND_Sim_mu_end_vx[ientry],ND_Sim_mu_end_vy,ND_Sim_mu_end_vz);
         //ND_Sim_mu_end_vx_af = eff->getRotMuEndV_AF_X();
         //ND_Sim_mu_end_vy_af = eff->getRotMuEndV_AF_Y();
         //ND_Sim_mu_end_vz_af = eff->getRotMuEndV_AF_Z();
