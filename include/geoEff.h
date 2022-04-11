0be45171ac3deeaec2175c15d92ec84f71209c71
FlynnGuo <93301049+FlynnYGUO@users.noreply.github.com>
2022-04-11 14:32:41 -0400

    updates

--- include/geoEff.h
+++ include/geoEff.h
@@ -120,10 +120,10 @@ class geoEff
   std::vector< float > getCurrentThrowDepsY(int i);
   std::vector< float > getCurrentThrowDepsZ(int i);
   // Transforms from On-Axis to Off-Axis
-  std::vector< float > getRotMuEndV(vector<double> RotMuEndV_BF, int dim);
+  std::vector< float > getRotMuEndV(int dim);
   std::vector< float > getRotMuEndV_AF_X();
-  std::vector< float > getRotMuEndV_AF_X();
-  std::vector< float > getRotMuEndV_AF_X();
+  std::vector< float > getRotMuEndV_AF_Y();
+  std::vector< float > getRotMuEndV_AF_Z();
 
   // Pass/fail for each set of vetoSize and vetoEnergy. Storing in TTree as uint64_t seems to take ~half the space of the equivalent vector< bool >.
   std::vector< std::vector< std::vector< uint64_t > > > getHadronContainmentThrows();
