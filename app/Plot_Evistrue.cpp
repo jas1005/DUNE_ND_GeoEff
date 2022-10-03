1323c2ddf4f7fadfb1ab22a0308e242d4eceab2b
FlynnGuo <93301049+FlynnYGUO@users.noreply.github.com>
2022-10-03 17:39:22 -0400

    test

--- app/Plot_Evistrue.cpp
+++ app/Plot_Evistrue.cpp
@@ -64,6 +64,7 @@ void Plot_Evistrue() // /pnfs/dune/persistent/users/flynnguo/myFDntuples/myntupl
   for ( int ientry = 0; ientry < nentries; ientry++ )
   {
     hist_E_vis_true->Fill(E_vis_true);
+    cout << "ientry: " <<ientry<< ", E_vis_true: " << E_vis_true << endl;
   }
 
   // Add labels;
