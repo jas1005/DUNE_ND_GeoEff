51dd3798e308b09750b66e0e2052bdcf1ccb453b
Wei Shi <wei.shi.1@stonybrook.edu>
2023-04-04 10:42:15 -0500

    update offaxis position signs

--- app/Helpers.h
+++ app/Helpers.h
@@ -40,7 +40,7 @@ double ND_off_axis_pos_stepsize = 2.5;   // unit meters
 // Average neutrino decay position in beam coordinates as a function of off axis x (from Luke)
 // This is used to interpolate the decay position
 double meanPDPZ[]               = {93.6072, 93.362,  90.346, 85.6266, 81.1443, 76.6664, 73.0865, 69.8348, 67.5822, 65.005, 62.4821, 60.8336, 59.1433, 57.7352}; // unit: meters
-double OffAxisPoints[]          = {-2,      0.5,     3,      5.5,     8,       10.5,    13,      15.5,    18,      20.5,   23,      25.5,    28,      30.5};
+double OffAxisPoints[]          = {2,       -0.5,    -3,     -5.5,    -8,      -10.5,   -13,     -15.5,   -18,     -20.5,  -23,     -25.5,   -28,     -30.5};
 double NDLarPos_new1[]        = {0., -28.}; // unit meter
 double NDLarPos_new2[]        = {-1.75, -25.75}; // unit meter
 double ND_Lar_dtctr_pos_new_stepsize = 4; // unit meter
